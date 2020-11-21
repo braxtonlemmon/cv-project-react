59978d6c6cc92c1a8baa9d3b48b27bbb8cc3aadc
Braxton Lemmon <braxtonlemmon@gmail.com>
2020-11-21 11:45:03 -0700

    Updates

---
+++ precache-manifest.874b24641f93908230cec8e39401c6fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966a0cc430a95b47124922a1d6ae2955",
+    "url": "/cv-project-react/index.html"
+  },
+  {
+    "revision": "530f80f9390cc5d780cf",
+    "url": "/cv-project-react/static/js/2.59af043d.chunk.js"
+  },
+  {
+    "revision": "5d4e16e2127fd1a83eb36d2b41e882c0",
+    "url": "/cv-project-react/static/js/2.59af043d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4406fcd62c68e869457",
+    "url": "/cv-project-react/static/js/main.395a489b.chunk.js"
+  },
+  {
+    "revision": "8018d96c1668d4d97eb9",
+    "url": "/cv-project-react/static/js/runtime-main.a502c854.js"
+  }
+]);
